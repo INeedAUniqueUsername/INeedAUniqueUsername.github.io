59866a997f3f951c6b133553cb88fd93d6906d32
INeedAUniqueUsername <achen115@ucr.edu>
2018-02-15 13:32:04 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 console.log('a');
 let ENTER_KEY_CODE = 13;
 document
-    .getElementById('newTask')
-    .addEventListener('keypress', function() {
-      console.log('Pressed Key');
-      if(event.which == ENTER_KEY_CODE) {
-          console.log(event.target.value);
-      }
+  .getElementById('newTask')
+  .addEventListener('keypress', function() {
+    console.log('Pressed Key');
+    if(event.which !== ENTER_KEY_CODE) {
+        return;
+    }
+    console.log(event.target.value);
+    let newTask = document.createElement('li');
+    let newTaskContent = event.target.value;
+    
+    newTask.textContent = newTaskContent;
+    
+    document.getElementById("todoList").append(newTask);
 });
