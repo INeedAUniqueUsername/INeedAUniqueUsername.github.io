6ec598be2d542213b1c90bace7e5711dbd1d2fae
INeedAUniqueUsername <achen115@ucr.edu>
2018-02-15 13:24:51 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log('a');
+let ENTER_KEY_CODE = 13;
+document
+    .getElementById('newTask')
+    .addEventListener('keypress', function() {
+      console.log('Pressed Key');
+      if(event.which == ENTER_KEY_CODE) {
+          console.log(event.target.value);
+      }
+});
